7fe10499ec9ef9609398c6b59e63a55c79c5800b
mdemisse <mahiasfaw@gmail.com>
2018-07-03 13:51:17 -0500

    Created zuul-gateway running on port 8762

--- zuul-gateway/src/main/java/com/app/ZuulGatewayApplication.java
+++ zuul-gateway/src/main/java/com/app/ZuulGatewayApplication.java
@@ -2,10 +2,13 @@ package com.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.zuul.EnableZuulServer;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
 
 @SpringBootApplication
-@EnableZuulServer
+@EnableDiscoveryClient
+@EnableZuulProxy
 public class ZuulGatewayApplication {
 
 	public static void main(String[] args) {
